fd38528c7dcfed2d9ba82f03e77ec03575d07ced
Salim ABDELLOUCHE <salimabdellouche@MacBook-Pro-de-Salim.local>
2024-09-27 10:49:00 +0100

    ajout d'annontations dans models.py

--- app/models.py
+++ app/models.py
@@ -1,20 +1,21 @@
 # poker/app/models.py
+from typing import List, Dict
 
-CARD_VALUES = {
+CARD_VALUES: Dict[int, str] = {
     2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9', 10: '10',
     11: 'J', 12: 'Q', 13: 'K', 14: 'A'
 }
 
-CARD_SUITS = {'♠', '♥', '♦', '♣'}
+CARD_SUITS: set[str] = {'♠', '♥', '♦', '♣'}
 
-SUIT_MAPPING = {
+SUIT_MAPPING: Dict[str, str] = {
     '♠': 's',
     '♥': 'h',
     '♦': 'd',
     '♣': 'c'
 }
 
-VALUE_MAPPING = {
+VALUE_MAPPING: Dict[str, str] = {
     '2': '2',
     '3': '3',
     '4': '4',
@@ -31,38 +32,38 @@ VALUE_MAPPING = {
 }
 
 class Card:
-    def __init__(self, value, suit):
+    def __init__(self, value: int, suit: str) -> None:
         if value not in CARD_VALUES:
             raise ValueError(f"Invalid card value: {value}")
         if suit not in CARD_SUITS:
             raise ValueError(f"Invalid card suit: {suit}")
-        self.value = value
-        self.suit = suit
+        self.value: int = value
+        self.suit: str = suit
     
-    def __str__(self):
+    def __str__(self) -> str:
         return f'{CARD_VALUES[self.value]}{self.suit}'
 
-    def to_phevaluate_format(self):
+    def to_phevaluate_format(self) -> str:
         return f'{VALUE_MAPPING[CARD_VALUES[self.value]]}{SUIT_MAPPING[self.suit]}'
 
 
 class Deck:
-    def __init__(self):
-        self.cards = [Card(value, suit) for value in CARD_VALUES for suit in CARD_SUITS]
-        self.burned_cards = []
+    def __init__(self) -> None:
+        self.cards: List[Card] = [Card(value, suit) for value in CARD_VALUES for suit in CARD_SUITS]
+        self.burned_cards: List[Card] = []
 
-    def shuffle(self):
+    def shuffle(self) -> None:
         import random
         random.shuffle(self.cards)
 
-    def burn_card(self):
+    def burn_card(self) -> None:
         burned_card = self.cards.pop(0)
         self.burned_cards.append(burned_card)
 
-    def deal(self, number_of_cards):
+    def deal(self, number_of_cards: int) -> List[Card]:
         if number_of_cards > len(self.cards):
             raise ValueError("Il ne reste pas assez de cartes dans le paquet.")
         return [self.cards.pop(0) for _ in range(number_of_cards)]
 
-    def remaining_cards(self):
+    def remaining_cards(self) -> int:
         return len(self.cards)
