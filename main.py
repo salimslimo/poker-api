c1fe41af527ff6314455e4c866cf0d67fb53fe26
Salim ABDELLOUCHE <salimabdellouche@MacBook-Pro-de-Salim.local>
2024-10-05 14:06:41 +0100

    ajout du host 0.0.0.0 + ajout de dépendances + ajout de Dockerfile

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from app.routes import app
 
 if __name__ == "__main__":
     print(f"Poker API - Version {VERSION}")
-    app.run(debug=True, port=5000)
+    app.run(debug=True, port=5000, host='0.0.0.0')
